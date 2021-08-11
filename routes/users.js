19d5a136f8d6cebb964eb420037a1dc391fe648b
Nikolay Shvaika <hiptoz@yandex.ru>
2021-08-11 18:46:20 +0300

    customEmailValidationStaged

--- routes/users.js
+++ routes/users.js
@@ -3,20 +3,12 @@ const { celebrate, Joi } = require('celebrate');
 const validator = require('validator');
 const { updateUser, getCurrentUser } = require('../controllers/userControllers');
 
-const method = (value) => {
-  const result = validator.isEmail(value);
-  if (result) {
-    return value;
-  }
-  throw new Error('Email validation err');
-};
-
 router.get('/users/me', getCurrentUser);
 
 router.patch('/users/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required(),
-    email: Joi.required().custom(method),
+    email: Joi.required(),
   }),
 }), updateUser);
 
